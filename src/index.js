0bbf0d5b04b7c957e02e6326d7a59e6a653fe972
Akashchandre <chandreakash@gmail.com>
2025-01-06 19:53:01 +0530

    testing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'; // React 18 API
 import { Provider } from 'react-redux';
 import store from './store.js';
 import './index.css';
-import App from './App';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
